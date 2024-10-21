fe47c3df4f2458d667963c148c77fc551d7d12dd
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-10-21 19:56:36 +0200

    created reusable button including styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import styled from "styled-components";
+import Button from "./ui/Button.jsx";
+import Input from "./ui/Input.jsx";
+import GlobalStyles from "./styles/GlobalStyles.js";
+import Heading from "./ui/Heading.jsx";
+import Row from "./ui/Row.jsx";
+
+const StyledApp = styled.div`
+  /* background-color: orangered; */
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check In and Out</Heading>
+              <Button onClick={() => alert("Check In")}>Check In</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Check Out")}
+              >
+                Check Out
+              </Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of Guests" />
+              <Input type="number" placeholder="Number of Guests" />
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
