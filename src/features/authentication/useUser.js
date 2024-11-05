20d423af64b628c7c312d7c255837ecc0a48a4ae
RazvanConstantin1 <razvanconstantin1097@gmail.com>
2024-11-05 05:58:14 +0200

    implemented authorization through the ProtectedRoute component

---
+++ src/features/authentication/useUser.js
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import { geCurrentUser } from "../../services/apiAuth.js";
+
+export function useUser() {
+  const { isLoading, data: user } = useQuery({
+    queryKey: ["user"],
+    queryFn: geCurrentUser,
+  });
+
+  return { isLoading, user, isAuthenticated: user?.role === "authenticated" };
+}
